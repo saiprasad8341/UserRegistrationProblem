b821dd6be1591f6bb770a5e93d04cc0eac6591bd
Saiprasad8341 <saiprasad.8341@gmail.com>
2022-12-13 15:08:28 +0530

    Welcome to the User Registration Problem..

---
+++ src/com/bridgelabz/UserRegistrationProblem.java
@@ -0,0 +1,7 @@
+package com.bridgelabz;
+
+public class UserRegistrationProblem {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the User Registration Problem...!!!");
+    }
+}
